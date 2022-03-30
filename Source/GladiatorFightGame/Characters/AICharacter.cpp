0b25ab1a766c64da6042ab2ec4184a1d6aa36302
gurgenkarapetyan <gurgen.karapetyan.noor@gmail.com>
2022-03-30 17:15:58 +0400

    - added AI Character

---
+++ Source/GladiatorFightGame/Characters/AICharacter.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AICharacter.h"
+
+#include "GladiatorFightGame/Items/PickableItemBase.h"
+#include "GladiatorFightGame/Items/PickableWeapon.h"
+
+void AAICharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	CreateAndAttachWeapons();
+}
+
+void AAICharacter::CreateAndAttachWeapons()
+{
+	FVector Position = GetActorLocation();
+	FRotator Rotation = GetActorRotation();
+
+	if (Shield)
+	{
+		APickableItemBase* NewShield = Cast<APickableItemBase>(GetWorld()->SpawnActor(Shield, &Position, &Rotation));
+		NewShield->AttachItemTo(GetMesh(), TEXT("DualWeaponPoint"));
+	}
+
+	
+	if (Weapon)
+	{
+		APickableWeapon* NewWeapon = Cast<APickableWeapon>(GetWorld()->SpawnActor(Weapon, &Position, &Rotation));
+		NewWeapon->AttachItemTo(GetMesh(), TEXT("WeaponPoint"));
+	}
+}
+
+void AAICharacter::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+}
+
+
---
+++ Source/GladiatorFightGame/Characters/AICharacter.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ArenaCharacter.h"
+#include "AICharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GLADIATORFIGHTGAME_API AAICharacter : public AArenaCharacter
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Tick(float DeltaSeconds) override;
+
+	void CreateAndAttachWeapons();
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(AllowPrivateAccess="true"))
+	TSubclassOf<APickableItemBase> Shield;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(AllowPrivateAccess="true"))
+	TSubclassOf<APickableWeapon> Weapon;
+};
